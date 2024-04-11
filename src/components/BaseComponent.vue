f494fc5a6bd49db08071a70e2de91808d5f95da6
Milan Vlaskovic <milan.vlaskovic@devtechgroup.com>
2024-04-11 23:56:46 +0200

    vue, simple store

---
+++ src/components/BaseComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="state">
+    <h3>{{ counter }}</h3>
+    <button @click="addOne">Add 1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseComponent',
+  methods: {
+    addOne() {
+      this.$store.state.counter ++;
+    }
+  },
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
